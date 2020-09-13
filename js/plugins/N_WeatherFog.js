4c4f242fffecda2cc6e6c2438f395a26f72b0ef0
Nolonar <69093381+Nolonar@users.noreply.github.com>
2020-09-13 18:40:32 +0200

    remove unnecessary functions

--- js/plugins/N_WeatherFog.js
+++ js/plugins/N_WeatherFog.js
@@ -219,16 +219,8 @@
             const int octaves = ${parameters.fogQuality};
             const vec3 noiseScale = vec3(${parameters.xScale}, ${parameters.yScale}, 1.0);
 
-            vec3 mod289(vec3 x) {
-                return x - floor(x * (1.0 / 289.0)) * 289.0;
-            }
-
-            vec4 mod289(vec4 x) {
-                return x - floor(x * (1.0 / 289.0)) * 289.0;
-            }
-
             vec4 permute(vec4 x) {
-                return mod289(((x * 34.0) + 1.0) * x);
+                return mod((x * 34.0 + 1.0) * x, 289.0);
             }
 
             vec4 taylorInvSqrt(vec4 r) {
@@ -238,7 +230,6 @@
             float snoise(vec3 v) {
                 const vec2 C = vec2(1.0 / 6.0, 1.0 / 3.0);
                 const vec4 D = vec4(0.0, 0.5, 1.0, 2.0);
-                const float n_ = 1.0 / 7.0;
                 vec3 i = floor(v + dot(v, C.yyy));
                 vec3 x0 = v - i + dot(i, C.xxx);
                 vec3 g = step(x0.yzx, x0.xyz);
@@ -248,9 +239,9 @@
                 vec3 x1 = x0 - i1 + C.xxx;
                 vec3 x2 = x0 - i2 + C.yyy;
                 vec3 x3 = x0 - D.yyy;
-                i = mod289(i);
+                i = mod(i, 289.0);
                 vec4 p = permute(permute(permute(i.z + vec4(0.0, i1.z, i2.z, 1.0)) + i.y + vec4(0.0, i1.y, i2.y, 1.0)) + i.x + vec4(0.0, i1.x, i2.x, 1.0));
-                vec3  ns = n_ * D.wyz - D.xzx;
+                vec3 ns = D.wyz / 7.0 - D.xzx;
                 vec4 j = p - 49.0 * floor(p * ns.z * ns.z);
                 vec4 x_ = floor(j * ns.z);
                 vec4 y_ = floor(j - 7.0 * x_);
@@ -264,17 +255,17 @@
                 vec4 sh = -step(h, vec4(0.0));
                 vec4 a0 = b0.xzyw + s0.xzyw * sh.xxyy;
                 vec4 a1 = b1.xzyw + s1.xzyw * sh.zzww;
-                vec3 p0 = vec3(a0.xy,h.x);
-                vec3 p1 = vec3(a0.zw,h.y);
-                vec3 p2 = vec3(a1.xy,h.z);
-                vec3 p3 = vec3(a1.zw,h.w);
-                vec4 norm = taylorInvSqrt(vec4(dot(p0,p0), dot(p1,p1), dot(p2, p2), dot(p3,p3)));
+                vec3 p0 = vec3(a0.xy, h.x);
+                vec3 p1 = vec3(a0.zw, h.y);
+                vec3 p2 = vec3(a1.xy, h.z);
+                vec3 p3 = vec3(a1.zw, h.w);
+                vec4 norm = taylorInvSqrt(vec4(dot(p0, p0), dot(p1, p1), dot(p2, p2), dot(p3, p3)));
                 p0 *= norm.x;
                 p1 *= norm.y;
                 p2 *= norm.z;
                 p3 *= norm.w;
                 vec4 m = max(0.6 - vec4(dot(x0, x0), dot(x1, x1), dot(x2, x2), dot(x3, x3)), 0.0);
-                m = m * m;
+                m *= m;
                 return 42.0 * dot(m * m, vec4(dot(p0, x0), dot(p1, x1), dot(p2, x2), dot(p3, x3)));
             }
 
